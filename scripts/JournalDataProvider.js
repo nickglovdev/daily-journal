e71900ae723c294b5f433278a7f349b9ea440ab3
nickglovdev <nickglovdev@gmail.com>
2020-03-17 11:25:24 -0500

    Added API folder and file. Moved the Objects from JournalDateProver to API File. It is now in JSON format.

--- scripts/JournalDataProvider.js
+++ scripts/JournalDataProvider.js
@@ -7,38 +7,16 @@
  */
 
 // This is the original data. Can't Touch This.
-const journal = [
-    {
-        id: 1,
-        date: "07/24/2025",
-        concept: "HTML & CSS",
-        entry: "We talked about HTML components and how to make grid layouts with Flexbox in CSS.",
-        mood: "Ok"
-    },
-    {
-        id: 2,
-        date: "07/28/2025",
-        concept: "HTML & CSS",
-        entry: "Got angry at CSS becuase it didn't work the way I wanted it to.",
-        mood: "Mad"
-    },
-    {
-        id: 3,
-        date: "07/30/2025",
-        concept: "HTML & CSS",
-        entry: "Figured our the JavaScript for dialong buttons, and feeling groovy.",
-        mood: "Groovy"
-    }
-]
+
 
 /*
     You export a function that provides a version of the
     raw data in the format that you want
 */
-export const useJournalEntries = () => {
-    const sortedByDate = journal.sort(
-        (currentEntry, nextEntry) =>
-            Date.parse(currentEntry.date) - Date.parse(nextEntry.date)
-    )
-    return sortedByDate
-}
+// export const useJournalEntries = () => {
+//     const sortedByDate = journal.sort(
+//         (currentEntry, nextEntry) =>
+//             Date.parse(currentEntry.date) - Date.parse(nextEntry.date)
+//     )
+//     return sortedByDate
+// }
